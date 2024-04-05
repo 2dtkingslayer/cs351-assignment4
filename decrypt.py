412094e733bbe1151c59d870573be6113d8072c2
Truong Dang <truongdang@Apples-MacBook-Pro.local>
2024-04-05 00:23:13 -0400

    add printFrequency, clean code

--- decrypt.py
+++ decrypt.py
@@ -2,7 +2,7 @@
 # Group 11
 # Assignment 4
 # Member 1: Truong Dang - tdd4 - 31558941
-# Member 2: Arnoldo Rivera - UCID - NJIT ID
+# Member 2: Arnoldo Rivera - ar72 - 31511245
 # Member 3: Nifesimi Akintola - UCID - NJIT ID
 # Member 4: Bruno Quinones - bq3 - 31551111
 
@@ -10,11 +10,14 @@
 def barLine():
     print("-" * 30)
 
+# Print Input String (Cipher Text)
+def printInput(input_string):
+    print("Input (Cipher Text): " + input_string)
+
 # Print Output String (Plain Text)
 def printOutput(output_string):
     print("Output (Plain Text): " + output_string)
-    barLine()
-
+    
 # Convert CT to PT using keys
 def convertToPlainText(cipher: str, keys: dict):
     output_string = ''
@@ -60,8 +63,13 @@ def updateKeys(combinedCP: dict, new_pairs: dict):
     tempcombinedCP = combinedCP.copy()
     for cipher_letter in new_pairs.keys():
         tempcombinedCP[cipher_letter] = new_pairs[cipher_letter]
-
     return tempcombinedCP
+
+def printFrequency(keys_dict: dict, freq_dict: dict):
+    print("Cipher Letter\tFrequency\tPlain Letter")
+    for key in keys_dict.keys():
+        print(f"{key}\t\t{freq_dict[key]}\t\t{keys_dict[key]}")
+    barLine()
 
 # Initiaizing Frequency Table
 frequency = {
@@ -82,46 +90,34 @@ barLine()
 # Analyze Frequency
 cipherTextFreq = {}
 for char in cipher:
-    if char in punc or char == ' ':
+    if char in punc or char == ' ' or not char.isalpha():
         continue
     if char in cipherTextFreq:
         cipherTextFreq[char] += 1
     else:
         cipherTextFreq[char] = 1
 # Sort Frequency (high to low)
-cipherTextFreq = sorted(cipherTextFreq, key=lambda x: cipherTextFreq[x], reverse=True)
+cipherTextFreqSort = sorted(cipherTextFreq, key=lambda x: cipherTextFreq[x], reverse=True)
 # Connect Cipher Text's Letters Frequency to Frequency Table
 combinedCtoP = {}
-for cipherTextLetter in cipherTextFreq:
+for cipherTextLetter in cipherTextFreqSort:
     combinedCtoP[cipherTextLetter] = frequency.pop()
-
-#print(combinedCtoP)
-#print("Cipher Keys: ", end=" ")
-#for letter in combinedCtoP.keys():
-#    print(letter, end=" | ")
-#print()
-#print("Plain Keys: ", end="  ")
-#for letter in combinedCtoP.values():
-#    print(letter, end=" | ")
+printFrequency(combinedCtoP, cipherTextFreq)
 
 output_string = convertToPlainText(cipher, combinedCtoP)
 
-
-
 printKeys(combinedCtoP)
-print("CT: " + cipher)
-print("PT: " + output_string)
+printInput(cipher)
+printOutput(output_string)
 
 # Manual Replacement
 ans = input("Would you like to manually replace some characters? (Y/N): ").upper()
 while ans not in ['Y', 'N']:
     ans = input("Enter a valid option (Y/N) or (E) to exit: ").upper()
-    if ans in ['E', 'N']:
+    if ans in ['E', 'e']:
         exit(0)
 if ans == 'N': # No Editing
     exit(0)
-
-barLine()
 
 while ans in ['Y', 'y']:
     barLine()
@@ -132,7 +128,9 @@ while ans in ['Y', 'y']:
     print("CT: " + cipher)
     output_string = convertToPlainText(cipher, combinedCtoP)
     print("PT: " + output_string)
-    ans = input("Would you like to manually replace some characters? (Y/N): ")
+    ans = input("Would you like to manually replace some characters? (Y/N): ").upper()
+    while ans not in ['Y', 'N']:
+        ans = input("Enter a valid option (Y/N): ").upper()
 
 barLine()
 print("Final Result of Plain Text: " + output_string)
